3987d9f74c2ff6fd76aeb666fbfee3bf37baf3b1
Potapter <potapter@mail.ru>
2024-04-04 17:54:24 +0300

    CLIENT-0000: Queen class

--- chess_gameplay/chess_game.py
+++ chess_gameplay/chess_game.py
@@ -23,3 +23,89 @@ class King(Figure):
                     if -1 < self.x + i < 8 and -1 < self.y + j < 8 and (board[self.x + i][self.y + j] == ' ' or board[self.x + i][self.y + j].isupper() != self.label.isupper()):
                         self.possible_moves.append((self.x + i, self.y + j))
 
+class Queen(Figure):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, color)
+        self.value = 8
+        if color == 'w':
+            self.label = 'Q'
+        elif color == 'b':
+            self.label = 'q'
+
+    def update_possible_moves(self, board):
+        self.possible_moves = []
+        
+        for i in range(1, min(self.x, 7 - self.y) + 1):
+            if board[self.x - i][self.y + i] == ' ':
+                self.possible_moves.append((self.x - i, self.y + i))
+            elif board[self.x - i][self.y + i].isupper() != self.label.isupper():
+                self.possible_moves.append((self.x - i, self.y + i))
+                break
+            else:
+                break
+        
+        for i in range(1, min(7 - self.x, 7 - self.y) + 1):
+            if board[self.x + i][self.y + i] == ' ':
+                self.possible_moves.append((self.x + i, self.y + i))
+            elif board[self.x + i][self.y + i].isupper() != self.label.isupper():
+                self.possible_moves.append((self.x + i, self.y + i))
+                break
+            else:
+                break
+        
+        for i in range(1, min(7 - self.x, self.y) + 1):
+            if board[self.x + i][self.y - i] == ' ':
+                self.possible_moves.append((self.x + i, self.y - i))
+            elif board[self.x + i][self.y - i].isupper() != self.label.isupper():
+                self.possible_moves.append((self.x + i, self.y - i))
+                break
+            else:
+                break
+        
+        for i in range(1, min(self.x, self.y) + 1):
+            if board[self.x - i][self.y - i] == ' ':
+                self.possible_moves.append((self.x - i, self.y - i))
+            elif board[self.x - i][self.y - i].isupper() != self.label.isupper():
+                self.possible_moves.append((self.x - i, self.y - i))
+                break
+            else:
+                break
+        
+        
+        for i in range(1, 8 - self.y):
+            if board[self.x][self.y + i] == ' ':
+                self.possible_moves.append((self.x, self.y + i))
+            elif board[self.x][self.y + i].isupper() != self.label.isupper():
+                self.possible_moves.append((self.x, self.y + i))
+                break
+            else:
+                break
+        
+        for i in range(1, 8 - self.x):
+            if board[self.x + i][self.y] == ' ':
+                self.possible_moves.append((self.x + i, self.y))
+            elif board[self.x + i][self.y].isupper() != self.label.isupper():
+                self.possible_moves.append((self.x + i, self.y))
+                break
+            else:
+                break
+        
+        for i in range(1, self.y + 1):
+            if board[self.x][self.y - i] == ' ':
+                self.possible_moves.append((self.x, self.y - i))
+            elif board[self.x][self.y - i].isupper() != self.label.isupper():
+                self.possible_moves.append((self.x, self.y - i))
+                break
+            else:
+                break
+        
+        for i in range(1, self.x + 1):
+            if board[self.x - i][self.y] == ' ':
+                self.possible_moves.append((self.x - i, self.y))
+            elif board[self.x - i][self.y].isupper() != self.label.isupper():
+                self.possible_moves.append((self.x - i, self.y))
+                break
+            else:
+                break
+
+
