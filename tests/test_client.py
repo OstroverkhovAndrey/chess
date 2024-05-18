f24ef5b5560813d52ba095dedd350aa8cd5430a3
OstroverkhovAndrey <andrey26052001@gmail.com>
2024-05-18 17:25:50 +0300

    TEST-0001: test for login command

--- tests/test_client.py
+++ tests/test_client.py
@@ -68,3 +68,39 @@ class TestRegistre(unittest.TestCase):
         self.client.do_registre("1")
         self.client.write_to_server.assert_called_with("registre 1", 3)
         chess_client.print.assert_called_with("Success registre")
+
+
+class TestLogin(unittest.TestCase):
+
+    def setUp(self):
+        self.client = chess_client.chess_client()
+        chess_client.print = MagicMock()
+        chess_client.chess_client.write_to_server = MagicMock()
+
+        def wait_request_ans(self, num):
+            self.request[num] = "success_login"
+        chess_client.chess_client.wait_request_ans = wait_request_ans
+
+        def wait_complet_ans(self, num):
+            self.complet[num] = "1 2 3"
+        chess_client.chess_client.wait_complet_ans = wait_complet_ans
+
+    def test_do_login(self):
+        self.client.do_login("1 1")
+        chess_client.print.assert_called_with("More arguments")
+        self.client.do_login("")
+        chess_client.print.assert_called_with("Not enough arguments")
+        self.client.do_login("#@$")
+        chess_client.print.assert_called_with("Incorrect name")
+
+        self.client.do_login("1")
+        self.client.write_to_server.assert_called_with("login 1", 3)
+        chess_client.print.assert_called_with("Success login")
+        self.client.do_login("1")
+        self.assertEqual(len(self.client.write_to_server.mock_calls), 1)
+        chess_client.print.assert_called_with("You already login")
+
+    def test_complet_login(self):
+        self.assertEqual(self.client.complete_login("", "login ", 6, 6),
+                         ["1", "2", "3"])
+        self.client.write_to_server.assert_called_with("offline_users", 4)
