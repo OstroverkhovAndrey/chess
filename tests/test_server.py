74a1110bfd595b870c94b6e1cb33f1469c9ee08a
OstroverkhovAndrey <andrey26052001@gmail.com>
2024-05-18 11:50:21 +0300

    TEST-0001: test for registre coroutine

--- tests/test_server.py
+++ tests/test_server.py
@@ -20,3 +20,35 @@ class TestGetMsgNum(unittest.TestCase):
         num, msg = chess_server.get_msg_num(" msg\n")
         self.assertEqual(num, 0)
         self.assertEqual(msg, msg)
+
+
+class TestServerRegistre(unittest.IsolatedAsyncioTestCase):
+
+    def setUp(self):
+        chess_server.dump_user_info = MagicMock()
+
+    async def asyncSetUp(self):
+        chess_server.send_msg = AsyncMock()
+        chess_server.users = {}
+
+    async def test_registre_ok(self):
+        self.assertFalse("user_name" in chess_server.users)
+
+        await chess_server.registre("user_name", "writer", "command_num")
+        self.assertTrue("user_name" in chess_server.users)
+        chess_server.send_msg.assert_called_with("writer", "command_num", "registre_ok")
+        chess_server.dump_user_info.assert_called_with(chess_server.users)
+
+    async def test_registre_not(self):
+        self.assertFalse("user_name" in chess_server.users)
+
+        await chess_server.registre("user_name", "writer", "command_num")
+        self.assertTrue("user_name" in chess_server.users)
+        chess_server.send_msg.assert_called_with("writer", "command_num", "registre_ok")
+        chess_server.dump_user_info.assert_called_with(chess_server.users)
+
+        await chess_server.registre("user_name", "writer", "command_num")
+        chess_server.send_msg.assert_called_with("writer", "command_num", "registre_not")
+        self.assertEqual(len(chess_server.send_msg.mock_calls), 2)
+        self.assertEqual(len(chess_server.dump_user_info.mock_calls), 1)
+
