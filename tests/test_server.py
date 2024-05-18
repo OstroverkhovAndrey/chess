4c599d21af7f8ebf67d661969d35cca91f413251
OstroverkhovAndrey <andrey26052001@gmail.com>
2024-05-18 11:41:22 +0300

    TEST-0001: test for get_msg_num

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,2 +1,22 @@
 
 """Tests for server."""
+import server.src.chess_server as chess_server
+import unittest
+from unittest.mock import AsyncMock, MagicMock
+from server.src.clients_info import ClientsInfo
+from server.src.user_info import UserInfo
+from server.src.games import GamesDict
+from server.src.game_history import GameHistory
+
+
+class TestGetMsgNum(unittest.TestCase):
+
+    def test_msg_with_num(self):
+        num, msg = chess_server.get_msg_num("12: msg\n")
+        self.assertEqual(num, 12)
+        self.assertEqual(msg, msg)
+
+    def test_msg_without_num(self):
+        num, msg = chess_server.get_msg_num(" msg\n")
+        self.assertEqual(num, 0)
+        self.assertEqual(msg, msg)
