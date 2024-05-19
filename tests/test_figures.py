9183548f2121d107df1ea98714c11406abdf1659
Potapter <potapter@mail.ru>
2024-05-19 17:57:41 +0300

    TEST-0003: Tests forchess_game and figures modules

--- tests/test_figures.py
+++ tests/test_figures.py
@@ -1,2 +1,289 @@
+"""Test of figures module"""
 
-"""Tetsts for chess figures."""
+import unittest
+import sys
+import os
+sys.path.insert(1, os.path.dirname(__file__) + '/../client/src')
+import figures
+
+class TestCoordinatesTranslators(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_coordinates_translators(self):
+        self.assertEqual(figures.coordinates_to_computer('e2'), (4, 1))
+        self.assertEqual(figures.coordinates_to_computer((4,1)), (4, 1))
+
+        self.assertEqual(figures.coordinates_to_human((4, 1)), 'e2')
+        self.assertEqual(figures.coordinates_to_human('e2'), 'e2')
+
+    def tearDown(self):
+        pass
+
+
+class TestKing(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_King(self):
+        figure = figures.King(4, 4, 'b')
+        self.assertEqual(figure.x, 4)
+        self.assertEqual(figure.y, 4)
+        self.assertEqual(figure.label, 'Kb')
+
+        figure = figures.King(4, 4, 'w')
+        self.assertEqual(figure.x, 4)
+        self.assertEqual(figure.y, 4)
+        self.assertEqual(figure.label, 'Kw')
+        
+        board = [[' ' for i in range(8)] for j in range(8)]
+        self.assertEqual(figure.get_possible_moves(board), ['d4', 'd5', 'd6', 'e4', 'e6', 'f4', 'f5', 'f6'])
+
+        board[3][4] = 'Pw'
+        board[4][3] = 'Pw'
+        board[5][4] = 'Pw'
+        board[4][5] = 'Pw'
+        self.assertEqual(figure.get_possible_moves(board), ['d4', 'd6', 'f4', 'f6'])
+        board[3][3] = 'Pw'
+        board[5][3] = 'Pw'
+        board[3][5] = 'Pw'
+        board[5][5] = 'Pw'
+        self.assertEqual(figure.get_possible_moves(board), [])
+
+        board = [[' ' for i in range(8)] for j in range(8)]
+        self.assertEqual(figure.get_possible_moves(board), ['d4', 'd5', 'd6', 'e4', 'e6', 'f4', 'f5', 'f6'])
+
+        board[3][4] = 'Pb'
+        board[4][3] = 'Pb'
+        board[5][4] = 'Pb'
+        board[4][5] = 'Pb'
+        self.assertEqual(figure.get_possible_moves(board), ['d4', 'd5', 'd6', 'e4', 'e6', 'f4', 'f5', 'f6'])
+        board[3][3] = 'Pb'
+        board[5][3] = 'Pb'
+        board[3][5] = 'Pb'
+        board[5][5] = 'Pb'
+        self.assertEqual(figure.get_possible_moves(board), ['d4', 'd5', 'd6', 'e4', 'e6', 'f4', 'f5', 'f6'])
+
+        figure1 = figures.King(4, 0, 'w')
+        figure2 = figures.King(4, 7, 'b')
+        board = [[' ' for i in range(8)] for j in range(8)]
+        board[0][0] = 'Rw'
+        board[7][0] = 'Rw'
+        board[0][7] = 'Rb'
+        board[7][7] = 'Rb'
+        self.assertEqual(figure1.get_possible_moves(board), ['c1', 'd1', 'd2', 'e2', 'f1', 'f2', 'g1'])
+        self.assertEqual(figure2.get_possible_moves(board), ['c8', 'd7', 'd8', 'e7', 'f7', 'f8', 'g8'])
+
+        figure = figures.King(4, 0, 'w')
+        board = [[' ' for i in range(8)] for j in range(8)]
+        board[0][0] = 'Rw'
+        board[1][0] = 'Knw'
+        board[7][0] = 'Rb'
+        self.assertEqual(figure.get_possible_moves(board), ['d1', 'd2', 'e2', 'f1', 'f2'])
+        board[6][0] = 'Knb'
+        figure.update_possible_moves(board)
+        self.assertEqual(figure.possible_moves, [(3, 0), (3, 1), (4, 1), (5, 0), (5, 1)])
+
+    def tearDown(self):
+        pass
+
+
+class TestQueen(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_Queen(self):
+        figure = figures.Queen(4, 4, 'b')
+        self.assertEqual(figure.x, 4)
+        self.assertEqual(figure.y, 4)
+        self.assertEqual(figure.label, 'Qb')
+
+        figure = figures.Queen(4, 4, 'w')
+        self.assertEqual(figure.x, 4)
+        self.assertEqual(figure.y, 4)
+        self.assertEqual(figure.label, 'Qw')
+        
+        board = [[' ' for i in range(8)] for j in range(8)]
+        self.assertEqual(figure.get_possible_moves(board), ['a1', 'a5', 'b2', 'b5', 'b8', 'c3', 'c5', 'c7', 'd4', 'd5', 'd6', 'e1', 'e2', 'e3', 'e4', 'e6', 'e7', 'e8', 'f4', 'f5', 'f6', 'g3', 'g5', 'g7', 'h2', 'h5', 'h8'])
+
+        board[2][4] = 'Pw'
+        board[4][2] = 'Pw'
+        board[6][4] = 'Pw'
+        board[4][6] = 'Pw'
+        self.assertEqual(figure.get_possible_moves(board), ['a1', 'b2', 'b8', 'c3', 'c7', 'd4', 'd5', 'd6', 'e4', 'e6', 'f4', 'f5', 'f6', 'g3', 'g7', 'h2', 'h8'])
+        board[2][2] = 'Pw'
+        board[6][2] = 'Pw'
+        board[2][6] = 'Pw'
+        board[6][6] = 'Pw'
+        self.assertEqual(figure.get_possible_moves(board), ['d4', 'd5', 'd6', 'e4', 'e6', 'f4', 'f5', 'f6'])
+
+        board = [[' ' for i in range(8)] for j in range(8)]
+        self.assertEqual(figure.get_possible_moves(board), ['a1', 'a5', 'b2', 'b5', 'b8', 'c3', 'c5', 'c7', 'd4', 'd5', 'd6', 'e1', 'e2', 'e3', 'e4', 'e6', 'e7', 'e8', 'f4', 'f5', 'f6', 'g3', 'g5', 'g7', 'h2', 'h5', 'h8'])
+
+        board[2][4] = 'Pb'
+        board[4][2] = 'Pb'
+        board[6][4] = 'Pb'
+        board[4][6] = 'Pb'
+        self.assertEqual(figure.get_possible_moves(board), ['a1', 'b2', 'b8', 'c3', 'c5', 'c7', 'd4', 'd5', 'd6', 'e3', 'e4', 'e6', 'e7', 'f4', 'f5', 'f6', 'g3', 'g5', 'g7', 'h2', 'h8'])
+        board[2][2] = 'Pb'
+        board[6][2] = 'Pb'
+        board[2][6] = 'Pb'
+        board[6][6] = 'Pb'
+        figure.update_possible_moves(board)
+        self.assertEqual(figure.possible_moves, [(2, 2), (2, 4), (2, 6), (3, 3), (3, 4), (3, 5), (4, 2), (4, 3), (4, 5), (4, 6), (5, 3), (5, 4), (5, 5), (6, 2), (6, 4), (6, 6)])
+
+    def tearDown(self):
+        pass
+
+
+class TestRook(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_Rook(self):
+        figure = figures.Rook(4, 4, 'b')
+        self.assertEqual(figure.x, 4)
+        self.assertEqual(figure.y, 4)
+        self.assertEqual(figure.label, 'Rb')
+
+        figure = figures.Rook(4, 4, 'w')
+        self.assertEqual(figure.x, 4)
+        self.assertEqual(figure.y, 4)
+        self.assertEqual(figure.label, 'Rw')
+        
+        board = [[' ' for i in range(8)] for j in range(8)]
+        self.assertEqual(figure.get_possible_moves(board), ['a5', 'b5', 'c5', 'd5', 'e1', 'e2', 'e3', 'e4', 'e6', 'e7', 'e8', 'f5', 'g5', 'h5'])
+
+        board[2][4] = 'Pw'
+        board[4][2] = 'Pw'
+        board[6][4] = 'Pw'
+        board[4][6] = 'Pw'
+        self.assertEqual(figure.get_possible_moves(board), ['d5', 'e4', 'e6', 'f5'])
+
+        board = [[' ' for i in range(8)] for j in range(8)]
+        self.assertEqual(figure.get_possible_moves(board), ['a5', 'b5', 'c5', 'd5', 'e1', 'e2', 'e3', 'e4', 'e6', 'e7', 'e8', 'f5', 'g5', 'h5'])
+
+        board[2][4] = 'Pb'
+        board[4][2] = 'Pb'
+        board[6][4] = 'Pb'
+        board[4][6] = 'Pb'
+
+        figure.update_possible_moves(board)
+        self.assertEqual(figure.possible_moves, [(2, 4), (3, 4), (4, 2), (4, 3), (4, 5), (4, 6), (5, 4), (6, 4)])
+
+    def tearDown(self):
+        pass
+
+class TestKnight(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_Knight(self):
+        figure = figures.Knight(4, 4, 'b')
+        self.assertEqual(figure.x, 4)
+        self.assertEqual(figure.y, 4)
+        self.assertEqual(figure.label, 'KNb')
+
+        figure = figures.Knight(4, 4, 'w')
+        self.assertEqual(figure.x, 4)
+        self.assertEqual(figure.y, 4)
+        self.assertEqual(figure.label, 'KNw')
+        
+        board = [[' ' for i in range(8)] for j in range(8)]
+        self.assertEqual(figure.get_possible_moves(board), ['c4', 'c6', 'd3', 'd7', 'f3', 'f7', 'g4', 'g6'])
+
+        board[2][3] = 'Pw'
+        board[2][5] = 'Pw'
+        board[6][3] = 'Pb'
+        board[6][5] = 'Pb'
+        figure.update_possible_moves(board)
+        self.assertEqual(figure.possible_moves, [(3, 2), (3, 6), (5, 2), (5, 6), (6, 3), (6, 5)])
+
+    def tearDown(self):
+        pass
+
+
+class TestBishop(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_Bishop(self):
+        figure = figures.Bishop(4, 4, 'b')
+        self.assertEqual(figure.x, 4)
+        self.assertEqual(figure.y, 4)
+        self.assertEqual(figure.label, 'Bb')
+
+        figure = figures.Bishop(4, 4, 'w')
+        self.assertEqual(figure.x, 4)
+        self.assertEqual(figure.y, 4)
+        self.assertEqual(figure.label, 'Bw')
+        
+        board = [[' ' for i in range(8)] for j in range(8)]
+        self.assertEqual(figure.get_possible_moves(board), ['a1', 'b2', 'b8', 'c3', 'c7', 'd4', 'd6', 'f4', 'f6', 'g3', 'g7', 'h2', 'h8'])
+
+        board[2][2] = 'Pw'
+        board[2][6] = 'Pw'
+        board[6][2] = 'Pb'
+        board[6][6] = 'Pb'
+        figure.update_possible_moves(board)
+        self.assertEqual(figure.possible_moves, [(3, 3), (3, 5), (5, 3), (5, 5), (6, 2), (6, 6)])
+
+    def tearDown(self):
+        pass
+
+
+class TestPawn(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_Pawn(self):
+        figure1 = figures.Pawn(4, 4, 'w')
+        self.assertEqual(figure1.x, 4)
+        self.assertEqual(figure1.y, 4)
+        self.assertEqual(figure1.label, 'Pw')
+
+        figure2 = figures.Pawn(4, 4, 'b')
+        self.assertEqual(figure2.x, 4)
+        self.assertEqual(figure2.y, 4)
+        self.assertEqual(figure2.label, 'Pb')
+        
+        board = [[' ' for i in range(8)] for j in range(8)]
+        self.assertEqual(figure1.get_possible_moves(board), ['e6'])
+        self.assertEqual(figure2.get_possible_moves(board), ['e4'])
+
+        figure1 = figures.Pawn(4, 1, 'w')
+        figure2 = figures.Pawn(4, 6, 'b')
+
+        board = [[' ' for i in range(8)] for j in range(8)]
+        self.assertEqual(figure1.get_possible_moves(board), ['e3', 'e4'])
+        self.assertEqual(figure2.get_possible_moves(board), ['e5', 'e6'])
+
+        board[3][2] = 'Pb'
+        board[5][2] = 'Pb'
+        board[3][5] = 'Pw'
+        board[5][5] = 'Pw'
+        figure1.update_possible_moves(board)
+        self.assertEqual(figure1.possible_moves, [(3, 2), (4, 2), (4, 3), (5, 2)])
+        figure2.update_possible_moves(board)
+        self.assertEqual(figure2.possible_moves, [(3, 5), (4, 4), (4, 5), (5, 5)])
+
+        figure1 = figures.Pawn(4, 4, 'w')
+        board = [[' ' for i in range(8)] for j in range(8)]
+        board[3][4] = 'Pb'
+        board[5][4] = 'Pb'
+        self.assertEqual(figure1.get_possible_moves(board), ['d6', 'e6', 'f6'])
+
+        figure2 = figures.Pawn(4, 3, 'b')
+        board = [[' ' for i in range(8)] for j in range(8)]
+        board[3][3] = 'Pw'
+        board[5][3] = 'Pw'
+        self.assertEqual(figure2.get_possible_moves(board), ['d3', 'e3', 'f3'])
+    def tearDown(self):
+        pass
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,2 +1,382 @@
+"""Test of chess_game module"""
 
-"""Tests chess game."""
+import unittest
+import sys
+import os
+sys.path.insert(1, os.path.dirname(__file__) + '/../client/src')
+from chess_game import Game
+
+
+class TestGameBeginning(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_game_beginning(self):
+        game = Game("w")
+        self.assertEqual(game.get_possible_moves(), {'a1': [],
+                                                     'a2': ['a3', 'a4'],
+                                                     'b1': ['a3', 'c3'],
+                                                     'b2': ['b3', 'b4'],
+                                                     'c1': [],
+                                                     'c2': ['c3', 'c4'],
+                                                     'd1': [],
+                                                     'd2': ['d3', 'd4'],
+                                                     'e1': [],
+                                                     'e2': ['e3', 'e4'],
+                                                     'f1': [],
+                                                     'f2': ['f3', 'f4'],
+                                                     'g1': ['f3', 'h3'],
+                                                     'g2': ['g3', 'g4'],
+                                                     'h1': [],
+                                                     'h2': ['h3', 'h4']})
+
+        self.assertTrue(game.isMyMove())
+        self.assertFalse(game.isDrawMove("e2", "e4"))
+        self.assertFalse(game.isWinMove("e2", "e4"))
+        self.assertTrue(game.isPossibleMove("e2", "e4"))
+        game.move("e2", "e4")
+
+        self.assertFalse(game.isMyMove())
+        game.move_from_server("e7", "e5")
+
+        self.assertTrue(game.isMyMove())
+        self.assertEqual(game.get_possible_moves()["e4"], [])
+
+    def tearDown(self):
+        pass
+
+
+class TestGetBoard(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_get_board(self):
+        game = Game("w")
+
+        self.assertEqual(game.get_board(), '\n   a    b    c    d    e    f    g    h\n ┌────┬────┬────┬────┬────┬────┬────┬────┐\n8│ Rb │KNb │ Bb │ Qb │ Kb │ Bb │KNb │ Rb │8\n ├────┼────┼────┼────┼────┼────┼────┼────┤\n7│ Pb │ Pb │ Pb │ Pb │ Pb │ Pb │ Pb │ Pb │7\n ├────┼────┼────┼────┼────┼────┼────┼────┤\n6│    │    │    │    │    │    │    │    │6\n ├────┼────┼────┼────┼────┼────┼────┼────┤\n5│    │    │    │    │    │    │    │    │5\n ├────┼────┼────┼────┼────┼────┼────┼────┤\n4│    │    │    │    │    │    │    │    │4\n ├────┼────┼────┼────┼────┼────┼────┼────┤\n3│    │    │    │    │    │    │    │    │3\n ├────┼────┼────┼────┼────┼────┼────┼────┤\n2│ Pw │ Pw │ Pw │ Pw │ Pw │ Pw │ Pw │ Pw │2\n ├────┼────┼────┼────┼────┼────┼────┼────┤\n1│ Rw │KNw │ Bw │ Qw │ Kw │ Bw │KNw │ Rw │1\n └────┴────┴────┴────┴────┴────┴────┴────┘\n   a    b    c    d    e    f    g    h\n')
+
+        self.assertTrue(game.isMyMove())
+        self.assertFalse(game.isDrawMove("e2", "e4"))
+        self.assertFalse(game.isWinMove("e2", "e4"))
+        self.assertTrue(game.isPossibleMove("e2", "e4"))
+        game.move("e2", "e4")
+
+        self.assertEqual(game.get_board(), '\n   a    b    c    d    e    f    g    h\n ┌────┬────┬────┬────┬────┬────┬────┬────┐\n8│ Rb │KNb │ Bb │ Qb │ Kb │ Bb │KNb │ Rb │8\n ├────┼────┼────┼────┼────┼────┼────┼────┤\n7│ Pb │ Pb │ Pb │ Pb │ Pb │ Pb │ Pb │ Pb │7\n ├────┼────┼────┼────┼────┼────┼────┼────┤\n6│    │    │    │    │    │    │    │    │6\n ├────┼────┼────┼────┼────┼────┼────┼────┤\n5│    │    │    │    │    │    │    │    │5\n ├────┼────┼────┼────┼────┼────┼────┼────┤\n4│    │    │    │    │ Pw │    │    │    │4\n ├────┼────┼────┼────┼────┼────┼────┼────┤\n3│    │    │    │    │    │    │    │    │3\n ├────┼────┼────┼────┼────┼────┼────┼────┤\n2│ Pw │ Pw │ Pw │ Pw │    │ Pw │ Pw │ Pw │2\n ├────┼────┼────┼────┼────┼────┼────┼────┤\n1│ Rw │KNw │ Bw │ Qw │ Kw │ Bw │KNw │ Rw │1\n └────┴────┴────┴────┴────┴────┴────┴────┘\n   a    b    c    d    e    f    g    h\n')
+
+    def tearDown(self):
+        pass
+
+
+class TestEat(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_eat(self):
+        game1 = Game("w")
+        game2 = Game("b")
+
+        self.assertFalse(game1.isDrawMove("e2", "e4"))
+        self.assertFalse(game1.isWinMove("e2", "e4"))
+        self.assertTrue(game1.isPossibleMove("e2", "e4"))
+        game1.move("e2", "e4")
+        game2.move_from_server("e2", "e4")
+
+        self.assertFalse(game2.isDrawMove("d7", "d5"))
+        self.assertFalse(game2.isWinMove("d7", "d5"))
+        self.assertTrue(game2.isPossibleMove("d7", "d5"))
+        game2.move("d7", "d5")
+        game1.move_from_server("d7", "d5")
+
+        self.assertFalse(game1.isDrawMove("e4", "d5"))
+        self.assertFalse(game1.isWinMove("e4", "d5"))
+        self.assertTrue(game1.isPossibleMove("e4", "d5"))
+        game1.move("e4", "d5")
+        game2.move_from_server("e4", "d5")
+
+        self.assertEqual(game1.board, [['Rw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'Rb'],
+                                      ['KNw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'KNb'],
+                                      ['Bw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'Bb'],
+                                      ['Qw', 'Pw', ' ', ' ', 'Pw', ' ', ' ', 'Qb'],
+                                      ['Kw', ' ', ' ', ' ', ' ', ' ', 'Pb', 'Kb'],
+                                      ['Bw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'Bb'],
+                                      ['KNw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'KNb'],
+                                      ['Rw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'Rb']])
+#        self.assertEqual(len(game1.white_figures), 16)
+#        self.assertEqual(len(game1.black_figures), 15)
+
+    def tearDown(self):
+        pass
+
+
+class TestEnPassant(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_en_passant(self):
+        game1 = Game("w")
+        game2 = Game("b")
+
+        self.assertFalse(game1.isDrawMove("e2", "e4"))
+        self.assertFalse(game1.isWinMove("e2", "e4"))
+        self.assertTrue(game1.isPossibleMove("e2", "e4"))
+        game1.move("e2", "e4")
+        game2.move_from_server("e2", "e4")
+
+        self.assertFalse(game2.isDrawMove("d7", "d5"))
+        self.assertFalse(game2.isWinMove("d7", "d5"))
+        self.assertTrue(game2.isPossibleMove("d7", "d5"))
+        game2.move("d7", "d5")
+        game1.move_from_server("d7", "d5")
+
+        self.assertFalse(game1.isDrawMove("e4", "e5"))
+        self.assertFalse(game1.isWinMove("e4", "e5"))
+        self.assertTrue(game1.isPossibleMove("e4", "e5"))
+        game1.move("e4", "e5")
+        game2.move_from_server("e4", "e5")
+
+        self.assertFalse(game2.isDrawMove("g7", "g5"))
+        self.assertFalse(game2.isWinMove("g7", "g5"))
+        self.assertTrue(game2.isPossibleMove("g7", "g5"))
+        game2.move("g7", "g5")
+        game1.move_from_server("g7", "g5")
+
+        self.assertFalse(game1.isDrawMove("e5", "d6"))
+        self.assertFalse(game1.isWinMove("e5", "d6"))
+        self.assertTrue(game1.isPossibleMove("e5", "d6"))
+        game1.move("e5", "d6")
+        game2.move_from_server("e5", "d6")
+
+        self.assertFalse(game2.isDrawMove("g5", "g4"))
+        self.assertFalse(game2.isWinMove("g5", "g4"))
+        self.assertTrue(game2.isPossibleMove("g5", "g4"))
+        game2.move("g5", "g4")
+        game1.move_from_server("g5", "g4")
+
+        self.assertFalse(game1.isDrawMove("h2", "h4"))
+        self.assertFalse(game1.isWinMove("h2", "h4"))
+        self.assertTrue(game1.isPossibleMove("h2", "h4"))
+        game1.move("h2", "h4")
+        game2.move_from_server("h2", "h4")
+
+        self.assertFalse(game2.isDrawMove("g4", "h3"))
+        self.assertFalse(game2.isWinMove("g4", "h3"))
+        self.assertTrue(game2.isPossibleMove("g4", "h3"))
+        game2.move("g4", "h3")
+        game1.move_from_server("g4", "h3")
+
+        self.assertEqual(game1.board, [['Rw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'Rb'],
+                                      ['KNw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'KNb'],
+                                      ['Bw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'Bb'],
+                                      ['Qw', 'Pw', ' ', ' ', ' ', 'Pw', ' ', 'Qb'],
+                                      ['Kw', ' ', ' ', ' ', ' ', ' ', 'Pb', 'Kb'],
+                                      ['Bw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'Bb'],
+                                      ['KNw', 'Pw', ' ', ' ', ' ', ' ', ' ', 'KNb'],
+                                      ['Rw', ' ', 'Pb', ' ', ' ', ' ', 'Pb', 'Rb']])
+#        self.assertEqual(len(game1.white_figures), 15)
+#        self.assertEqual(len(game1.black_figures), 15)
+
+    def tearDown(self):
+        pass
+
+
+
+class TestWin(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_win(self):
+        game1 = Game("w")
+        game2 = Game("b")
+
+        self.assertFalse(game1.isDrawMove("g2", "g4"))
+        self.assertFalse(game1.isWinMove("g2", "g4"))
+        self.assertTrue(game1.isPossibleMove("g2", "g4"))
+        game1.move("g2", "g4")
+        game2.move_from_server("g2", "g4")
+
+        self.assertFalse(game2.isDrawMove("e7", "e5"))
+        self.assertFalse(game2.isWinMove("e7", "e5"))
+        self.assertTrue(game2.isPossibleMove("e7", "e5"))
+        game2.move("e7", "e5")
+        game1.move_from_server("e7", "e5")
+
+        self.assertFalse(game1.isDrawMove("f2", "f3"))
+        self.assertFalse(game1.isWinMove("f2", "f3"))
+        self.assertTrue(game1.isPossibleMove("f2", "f3"))
+        game1.move("f2", "f3")
+        game2.move_from_server("f2", "f3")
+
+        self.assertFalse(game2.isDrawMove("d8", "h4"))
+        self.assertTrue(game2.isWinMove("d8", "h4"))
+        self.assertTrue(game2.isPossibleMove("d8", "h4"))
+        game2.move("d8", "h4")
+        game1.move_from_server("d8", "h4")
+
+        self.assertEqual(game1.board, [['Rw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'Rb'],
+                                       ['KNw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'KNb'],
+                                       ['Bw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'Bb'],
+                                       ['Qw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', ' '],
+                                       ['Kw', 'Pw', ' ', ' ', 'Pb', ' ', ' ', 'Kb'],
+                                       ['Bw', ' ', 'Pw', ' ', ' ', ' ', 'Pb', 'Bb'],
+                                       ['KNw', ' ', ' ', 'Pw', ' ', ' ', 'Pb', 'KNb'],
+                                       ['Rw', 'Pw', ' ', 'Qb', ' ', ' ', 'Pb', 'Rb']])
+
+    def tearDown(self):
+        pass
+
+
+class TestRightRoque(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_right_roque(self):
+        game1 = Game("w")
+        game2 = Game("b")
+
+        self.assertFalse(game1.isDrawMove("e2", "e4"))
+        self.assertFalse(game1.isWinMove("e2", "e4"))
+        self.assertTrue(game1.isPossibleMove("e2", "e4"))
+        game1.move("e2", "e4")
+        game2.move_from_server("e2", "e4")
+
+        self.assertFalse(game2.isDrawMove("e7", "e5"))
+        self.assertFalse(game2.isWinMove("e7", "e5"))
+        self.assertTrue(game2.isPossibleMove("e7", "e5"))
+        game2.move("e7", "e5")
+        game1.move_from_server("e7", "e5")
+
+        self.assertFalse(game1.isDrawMove("f1", "d3"))
+        self.assertFalse(game1.isWinMove("f1", "d3"))
+        self.assertTrue(game1.isPossibleMove("f1", "d3"))
+        game1.move("f1", "d3")
+        game2.move_from_server("f1", "d3")
+
+        self.assertFalse(game2.isDrawMove("f8", "d6"))
+        self.assertFalse(game2.isWinMove("f8", "d6"))
+        self.assertTrue(game2.isPossibleMove("f8", "d6"))
+        game2.move("f8", "d6")
+        game1.move_from_server("f8", "d6")
+
+        self.assertFalse(game1.isDrawMove("g1", "f3"))
+        self.assertFalse(game1.isWinMove("g1", "f3"))
+        self.assertTrue(game1.isPossibleMove("g1", "f3"))
+        game1.move("g1", "f3")
+        game2.move_from_server("g1", "f3")
+
+        self.assertFalse(game2.isDrawMove("g8", "f6"))
+        self.assertFalse(game2.isWinMove("g8", "f6"))
+        self.assertTrue(game2.isPossibleMove("g8", "f6"))
+        game2.move("g8", "f6")
+        game1.move_from_server("g8", "f6")
+
+        self.assertFalse(game1.isDrawMove("e1", "g1"))
+        self.assertFalse(game1.isWinMove("e1", "g1"))
+        self.assertTrue(game1.isPossibleMove("e1", "g1"))
+        game1.move("e1", "g1")
+        game2.move_from_server("e1", "g1")
+
+        self.assertFalse(game2.isDrawMove("e8", "g8"))
+        self.assertFalse(game2.isWinMove("e8", "g8"))
+        self.assertTrue(game2.isPossibleMove("e8", "g8"))
+        game2.move("e8", "g8")
+        game1.move_from_server("e8", "g8")
+
+        self.assertEqual(game1.board, [['Rw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'Rb'],
+                                       ['KNw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'KNb'],
+                                       ['Bw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'Bb'],
+                                       ['Qw', 'Pw', 'Bw', ' ', ' ', 'Bb', 'Pb', 'Qb'],
+                                       [' ', ' ', ' ', 'Pw', 'Pb', ' ', ' ', ' '],
+                                       ['Rw', 'Pw', 'KNw', ' ', ' ', 'KNb', 'Pb', 'Rb'],
+                                       ['Kw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'Kb'],
+                                       [' ', 'Pw', ' ', ' ', ' ', ' ', 'Pb', ' ']])
+
+    def tearDown(self):
+        pass
+
+
+class TestLeftRoque(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_left_roque(self):
+        game1 = Game("w")
+        game2 = Game("b")
+
+        self.assertFalse(game1.isDrawMove("d2", "d4"))
+        self.assertFalse(game1.isWinMove("d2", "d4"))
+        self.assertTrue(game1.isPossibleMove("d2", "d4"))
+        game1.move("d2", "d4")
+        game2.move_from_server("d2", "d4")
+
+        self.assertFalse(game2.isDrawMove("d7", "d5"))
+        self.assertFalse(game2.isWinMove("d7", "d5"))
+        self.assertTrue(game2.isPossibleMove("d7", "d5"))
+        game2.move("d7", "d5")
+        game1.move_from_server("d7", "d5")
+
+        self.assertFalse(game1.isDrawMove("c1", "e3"))
+        self.assertFalse(game1.isWinMove("c1", "e3"))
+        self.assertTrue(game1.isPossibleMove("c1", "e3"))
+        game1.move("c1", "e3")
+        game2.move_from_server("c1", "e3")
+
+        self.assertFalse(game2.isDrawMove("c8", "e6"))
+        self.assertFalse(game2.isWinMove("c8", "e6"))
+        self.assertTrue(game2.isPossibleMove("c8", "e6"))
+        game2.move("c8", "e6")
+        game1.move_from_server("c8", "e6")
+
+        self.assertFalse(game1.isDrawMove("b1", "c3"))
+        self.assertFalse(game1.isWinMove("b1", "c3"))
+        self.assertTrue(game1.isPossibleMove("b1", "c3"))
+        game1.move("b1", "c3")
+        game2.move_from_server("b1", "c3")
+
+        self.assertFalse(game2.isDrawMove("b8", "c6"))
+        self.assertFalse(game2.isWinMove("b8", "c6"))
+        self.assertTrue(game2.isPossibleMove("b8", "c6"))
+        game2.move("b8", "c6")
+        game1.move_from_server("b8", "c6")
+
+        self.assertFalse(game1.isDrawMove("d1", "d2"))
+        self.assertFalse(game1.isWinMove("d1", "d2"))
+        self.assertTrue(game1.isPossibleMove("d1", "d2"))
+        game1.move("d1", "d2")
+        game2.move_from_server("d1", "d2")
+
+        self.assertFalse(game2.isDrawMove("d8", "d7"))
+        self.assertFalse(game2.isWinMove("d8", "d7"))
+        self.assertTrue(game2.isPossibleMove("d8", "d7"))
+        game2.move("d8", "d7")
+        game1.move_from_server("d8", "d7")
+
+        self.assertFalse(game1.isDrawMove("e1", "c1"))
+        self.assertFalse(game1.isWinMove("e1", "c1"))
+        self.assertTrue(game1.isPossibleMove("e1", "c1"))
+        game1.move("e1", "c1")
+        game2.move_from_server("e1", "c1")
+
+        self.assertFalse(game2.isDrawMove("e8", "c8"))
+        self.assertFalse(game2.isWinMove("e8", "c8"))
+        self.assertTrue(game2.isPossibleMove("e8", "c8"))
+        game2.move("e8", "c8")
+        game1.move_from_server("e8", "c8")
+
+        self.assertEqual(game1.board, [[' ', 'Pw', ' ', ' ', ' ', ' ', 'Pb', ' '],
+                                       [' ', 'Pw', ' ', ' ', ' ', ' ', 'Pb', ' '],
+                                       ['Kw', 'Pw', 'KNw', ' ', ' ', 'KNb', 'Pb', 'Kb'],
+                                       ['Rw', 'Qw', ' ', 'Pw', 'Pb', ' ', 'Qb', 'Rb'],
+                                       [' ', 'Pw', 'Bw', ' ', ' ', 'Bb', 'Pb', ' '],
+                                       ['Bw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'Bb'],
+                                       ['KNw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'KNb'],
+                                       ['Rw', 'Pw', ' ', ' ', ' ', ' ', 'Pb', 'Rb']])
+
+    def tearDown(self):
+        pass
