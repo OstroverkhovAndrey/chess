4b60c73ca3cb44f7e3cc4d234784e8db332c2052
OstroverkhovAndrey <andrey26052001@gmail.com>
2024-05-20 14:58:31 +0300

    INFRASTRUCTURE-0009: update flake8

--- client/src/__init__.py
+++ client/src/__init__.py
@@ -1,11 +1,15 @@
 
+"""Init."""
+
 import os
 import sys
 sys.path.insert(1, os.path.dirname(__file__))
 from chess_client import chess_client
 from internationalization import _
 
+
 def client():
+    """Run client."""
     print("Run client!")
     try:
         chess_client().cmdloop()
